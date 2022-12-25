cd16337e698015ab6860ec617bd094ef52d6ad36
blaszkaaa <58654924+blaszkaaa@users.noreply.github.com>
2022-12-25 18:34:18 +0100

    1

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,11 +8,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //grid dimensions
+  int rowSize = 10;
+  int totalNumberOfSquares = 100;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Column(
         children: [
+          //high scores
+          Expanded(
+            child: Container(),
+          ),
+          //game grid
+          Expanded(
+            flex: 3,
+            child: GridView.builder(
+                itemCount: totalNumberOfSquares,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: rowSize,
+                ),
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[800],
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                    ),
+                  );
+                }),
+          ),
+          //play button
           Expanded(
             child: Container(),
           ),
